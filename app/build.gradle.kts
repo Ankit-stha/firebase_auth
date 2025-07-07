6d712a4b6b008d5cbfcbd9e705358dca90a233bd
ankeetshrestha <shresthaankit016@gmail.com>
2025-07-07 20:16:23 +0545

    all done

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,25 +39,23 @@ android {
 
 dependencies {
     // 🔽 Firebase BoM to manage all Firebase versions
+
+
+
+        implementation(libs.appcompat)
+        implementation(libs.material)
+        implementation(libs.activity)
+        implementation(libs.constraintlayout)
+        implementation(libs.navigation.fragment)
+        implementation(libs.navigation.ui)
+        testImplementation(libs.junit)
+        androidTestImplementation(libs.ext.junit)
+        androidTestImplementation(libs.espresso.core)
+        implementation("de.hdodenhof:circleimageview:3.1.0")
     implementation(platform("com.google.firebase:firebase-bom:33.0.0"))
     implementation("com.google.firebase:firebase-auth")
 
-    // 🔽 AndroidX libraries (use your preferred versions)
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
-    implementation("androidx.activity:activity:1.8.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.navigation:navigation-fragment:2.7.7")
-    implementation("androidx.navigation:navigation-ui:2.7.7")
+    }
 
-    // 🔽 Third-party libraries
-    implementation("de.hdodenhof:circleimageview:3.1.0")
-
-    // 🔽 Test dependencies
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-}
-apply(plugin = "com.google.gms.google-services")
 
 
